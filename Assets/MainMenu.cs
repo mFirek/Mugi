1095a504dd04ed496c8e34207879c3d703660b5e
hgrzes2411 <hubert.grzeskiewicz@outlook.com>
2024-03-18 19:58:25 +0100

    Dodanie podstawowej funkcjonalności do Prototypowego UI
    Dodano okno menu Load Game i Options wraz z działającym przyciskiem powrotu
    Dodano funkcjonalność do przycisku Quit Game, który wyłącza grę

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,11 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("SampleScene");
     }
+
+
+    public void QuitGame()
+        {
+        Application.Quit();
+
+    }
 }

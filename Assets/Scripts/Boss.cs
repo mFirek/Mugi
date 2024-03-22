eeda5e929f1ecf4372fc43233f53aa8b8a166817
Krzemen <92109470+Krzemen@users.noreply.github.com>
2024-03-22 15:38:23 +0100

    Zmiana do bossa i atak co strzela z dupy

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-
     public Transform player;
+  //  public GameObject laserPrefab;
+   // public Transform laserSpawnPoint;
 
     public bool isFlipped = false;
+
+   // public float fireRate = 0.5f; // Okreœl, jak czêsto boss strzela
+ //   private float nextFireTime = 0f;
+ 
+ //   public float laserLifetime = 1f; // Czas ¿ycia lasera
+
+   // private GameObject activeLaser; // Referencja do aktywnego lasera
+
+  //  void Update()
+    //{
+   //     LookAtPlayer();
+    //    if (Time.time >= nextFireTime)
+    //    {
+   //         Shoot();
+         //   nextFireTime = Time.time + 1f / fireRate;
+    //    }
+   // }
 
     public void LookAtPlayer()
     {
@@ -28,4 +46,23 @@ public class Boss : MonoBehaviour
         }
     }
 
+   // void Shoot()
+   // {
+        // Upewnij siê, ¿e nie strzelamy, gdy laser jest aktywny
+      //  if (activeLaser == null)
+      //  {
+            // Pobierz wysokoœæ punktu spawnu lasera
+      //      float laserHeight = laserSpawnPoint.position.y;
+            // Skoryguj pozycjê lasera o tê wysokoœæ
+      //      Vector3 laserSpawnPosition = new Vector3(laserSpawnPoint.position.x, laserHeight, laserSpawnPoint.position.z);
+
+            // Wystrzel laser z poprawionej pozycji
+       //     activeLaser = Instantiate(laserPrefab, laserSpawnPosition, laserSpawnPoint.rotation, transform);
+            // Ustawienie kierunku lasera w stronê gracza (jeœli potrzebne)
+      //      activeLaser.transform.right = (player.position - laserSpawnPosition).normalized;
+
+            // Zniszcz laser po czasie laserLifetime
+        //    Destroy(activeLaser, laserLifetime);
+       // }
+   // }
 }

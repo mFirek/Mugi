ab709e6cff9dd99fa47869d6726561645d4198c5
Krzemen <92109470+Krzemen@users.noreply.github.com>
2024-04-10 17:14:00 +0200

    poprawa skryptów,layerów i coliderów

--- Assets/Scripts/Bullet3.cs
+++ Assets/Scripts/Bullet3.cs
@@ -7,14 +7,19 @@ public class Bullet3 : MonoBehaviour
     public float bulletSpeed = 10f;
     private Rigidbody2D rb;
 
- 
 
-   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // Destroy the bullet after 2 seconds
+        Destroy(gameObject, 2f);
+    }
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
       
-        if (collision.gameObject.CompareTag("Teren"))
+        if (collision.gameObject.CompareTag("Teren") | collision.gameObject.CompareTag("Ceiling"))
         {
             Destroy(gameObject); // Destroy the bullet upon collision with an obstacle
         }

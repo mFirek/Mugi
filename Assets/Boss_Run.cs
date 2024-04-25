4a200935a4eb9065683337f1dc52560586e37944
hgrzes2411 <hubert.grzeskiewicz@outlook.com>
2024-04-25 14:53:52 +0200

    Zmieniono sposób opóźnienia wystrzału golema
    Zastosowano sposób z wykorzystaniem corutine

--- Assets/Boss_Run.cs
+++ Assets/Boss_Run.cs
@@ -1,43 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boss_Run : StateMachineBehaviour
 {
+    private MonoBehaviour monoBehaviour = null;
+
+    public Boss_Run(MonoBehaviour monoBehaviour)
+    {
+        this.monoBehaviour = monoBehaviour;
+    }
+
     public float speed = 2.5f;
     public float attackRange = 3f;
     public float RangeShoot = 30f;
-    public float rangeShootCooldown = 3f; // Czas oczekiwania miêdzy kolejnymi atakami na dystansie
-    public float rangeShootTimer = 0f; // Licznik czasu dla cooldownu
-    private float cooldownTimer = 0f; // Licznik czasu dla cooldownu
-    private bool cooldownActive = false; // Flaga wskazuj¹ca, czy cooldown jest aktywny
+    public float rangeShootCooldown = 3f;
+    public float rangeShootTimer = 0f;
+    private float cooldownTimer = 0f;
+    private bool cooldownActive = false;
 
-    public Transform firePoint; // Miejsce, z którego ma byæ wystrzelony pocisk
-    public GameObject bulletPrefab; // Prefabrykat pocisku
+    public Transform firePoint;
+    public GameObject bulletPrefab;
 
     Transform player;
     Rigidbody2D rb;
     Boss boss;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = animator.GetComponent<Rigidbody2D>();
-        boss = animator.GetComponent<Boss>();
+        this.monoBehaviour.StartCoroutine(WaitSomeThenResetStuff(animator));
+    }
 
-        // Zresetuj timer cooldownu
+    private IEnumerator WaitSomeThenResetStuff(Animator animator)
+    {
+        yield return new WaitForSeconds(1.0f);
+
         rangeShootTimer = 0f;
         cooldownActive = false;
 
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
         firePoint = GameObject.FindGameObjectWithTag("FirePoint").transform;
-
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         boss.LookAtPlayer();
 
@@ -45,18 +51,14 @@ public class Boss_Run : StateMachineBehaviour
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
 
-        // Aktualizuj timer cooldownu
         rangeShootTimer += Time.deltaTime;
 
-        // SprawdŸ, czy player jest w zasiêgu ataku na dystansie
         if (Vector2.Distance(player.position, rb.position) <= attackRange)
         {
             animator.SetTrigger("Attack");
             cooldownActive = true;
-            // Zresetuj timer cooldownu
             cooldownTimer = 0f;
         }
-        // SprawdŸ, czy cooldown dla ataku na dystansie zosta³ zakoñczony
         else if (rangeShootTimer >= rangeShootCooldown)
         {
             animator.SetTrigger("Shoot");
@@ -67,14 +69,12 @@ public class Boss_Run : StateMachineBehaviour
         if (cooldownActive)
         {
             cooldownTimer += Time.deltaTime;
-            // SprawdŸ, czy cooldown zosta³ zakoñczony
             if (cooldownTimer >= rangeShootCooldown)
             {
                 cooldownActive = false;
             }
         }
     }
-
 
     public void Shoot()
     {
@@ -86,19 +86,13 @@ public class Boss_Run : StateMachineBehaviour
         {
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
-            //miejsce opóŸnienia wystrza³u wzgledem animacji
-            Thread.Sleep(1500);
             rbBullet.AddForce(firePoint.up * 0.2f, ForceMode2D.Impulse);
         }
     }
 
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("Attack");
         animator.ResetTrigger("Shoot");
     }
 }
-
-

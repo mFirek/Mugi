4a91cd8853c908d0c0b91fa22ace0d61f9dd06ab
hgrzes2411 <hubert.grzeskiewicz@outlook.com>
2024-06-11 21:34:15 +0200

    Poprawa systemu wyboru leveli
    Podpięto levele(działają XD)

--- Assets/Selection Level Scripts/LevelSelectionMenuManager.cs
+++ Assets/Selection Level Scripts/LevelSelectionMenuManager.cs
@@ -1,39 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelectionMenuManager : MonoBehaviour
 {
-    public LevelObject[] levelObjects;
+    public LevelObject[] levelObjects; // Tablica z obiektami poziomów
+    public string[] levelSceneNames; // Tablica z nazwami scen dla poziomów
     public static int currLevel;
     public static int UnlockedLevels;
+
     public void OnClickLevel(int levelNum)
-    {   
+    {
         currLevel = levelNum;
-        SceneManager.LoadScene("GameScene");
+        if (levelNum >= 0 && levelNum < levelSceneNames.Length)
+        {
+            string sceneName = levelSceneNames[levelNum]; // Pobieramy nazwê sceny z tablicy
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Nieprawid³owy numer poziomu: " + levelNum);
+        }
     }
 
     public void OnClickBack()
     {
         this.gameObject.SetActive(false);
     }
-   
+
     void Start()
     {
         UnlockedLevels = PlayerPrefs.GetInt("UnlockedLevels", 0);
-        for(int i = 0;i<levelObjects.Length;i++)
+        for (int i = 0; i < levelObjects.Length; i++)
         {
-            if (UnlockedLevels>=i)
+            if (UnlockedLevels >= i)
             {
                 levelObjects[i].levelButton.interactable = true;
+                int levelIndex = i; // Tworzymy lokaln¹ kopiê indeksu, aby unikn¹æ problemów z przekazywaniem lambdy
+                levelObjects[i].levelButton.onClick.AddListener(() => OnClickLevel(levelIndex));
             }
         }
-    }
-
-   
-    void Update()
-    {
-        
     }
 }

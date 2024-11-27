b16048901c6dc5f78d654a23c410e2d1c8f586b5
Firek <wojtekman6969@gmail.com>
2024-11-27 19:40:36 +0100

    poprawki

--- Assets/Scripts/ReloadSceneOnTrigger.cs
+++ Assets/Scripts/ReloadSceneOnTrigger.cs
@@ -1,20 +1,46 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class ReloadSceneOnTrigger : MonoBehaviour
 {
+
+
     // Ta funkcja zostanie wywo³ana, gdy inny obiekt wejdzie w kolizjê z triggerem
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Mo¿esz dodaæ warunki, które sprawdz¹, czy obiekt, który wszed³ w trigger, to odpowiedni obiekt
-        // Przyk³ad: if (other.CompareTag("Player"))
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isDead)
         {
+            isDead = true;  // Zapobiegaj wielokrotnemu zliczaniu zgonów
             // Pobiera nazwê aktualnej sceny
             string currentSceneName = SceneManager.GetActiveScene().name;
 
             // Prze³adowuje aktualn¹ scenê
             SceneManager.LoadScene(currentSceneName);
+
+
+            // Mo¿esz dodaæ warunki, które sprawdz¹, czy obiekt, który wszed³ w trigger, to odpowiedni obiekt
+            // Przyk³ad: if (other.CompareTag("Player"))
+            if (GameEventsManager.instance != null)
+            {
+                GameEventsManager.instance.PlayerDied();
+            }
+            else
+            {
+                Debug.LogError("Nie znaleziono instancji GameEventsManager!");
+            }
+
+            StartCoroutine(ResetDeathFlag());
         }
     }
+
+    private bool isDead = false;  // Flaga zapobiegaj¹ca wielokrotnemu zliczaniu zgonów
+
+    IEnumerator ResetDeathFlag()
+    {
+        yield return new WaitForSeconds(1f);  // Poczekaj 1 sekundê przed resetowaniem flagi
+        isDead = false;  // Resetuj flagê
+    }
 }
+
